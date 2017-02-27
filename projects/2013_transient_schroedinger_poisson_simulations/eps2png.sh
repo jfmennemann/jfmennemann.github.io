7681fc95ebd5824338cab89fa5081dcee32e35b0
jfmennemann <jfmennemann@gmx.de>
2017-02-27 10:05:02 +0100

    minor changes

--- projects/2013_transient_schroedinger_poisson_simulations/eps2png.sh
+++
@@ -1,2 +0,0 @@
-# convert -alpha Off -density 600 -geometry x200 -gravity center -extent 250x200 oscillator_circuit_layout.eps oscillator_circuit_layout.jpg
-convert -alpha Off -density 1200 -resize 340x oscillator_circuit_layout.eps oscillator_circuit_layout.png
--- projects/2015_optimal_control_of_becs_in_three_dimensions/resize.sh
+++
@@ -1,1 +0,0 @@
-convert -alpha Off -density 2048 -resize 340x version_1.png version_1_small.png
